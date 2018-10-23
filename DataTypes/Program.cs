b487a003eb3eb81aa4bb47a29a42cb6a4f633905
James Webber <James.Webber@cansfordlabs.co.uk>
2018-10-23 08:15:07 +0100

    Ex1

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataTypes
 {
@@ -10,6 +6,24 @@ namespace DataTypes
     {
         static void Main(string[] args)
         {
+            // 1. Write a C# Sharp program that takes three letters as input and display them in reverse order.
+            Ex1(Ask("Give me at least 3 letters: "));
+
+
+
+            Console.ReadLine();
+        }
+        static string Ask(string question)
+        {
+            Console.Write(question);
+            return Console.ReadLine();
+        }
+        static void Ex1(string letters)
+        {
+            char[] chars = letters.ToCharArray();
+            Array.Reverse(chars);
+            Console.Write(string.Join(null, chars));
         }
     }
+
 }
